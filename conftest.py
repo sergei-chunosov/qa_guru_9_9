78789989b91b9e7ecdcf6100bd3ba8dc13ab7d84
necros <necros83@gmail.com>
2023-12-23 20:55:32 +0700

    add changes to conftest selenoid

--- conftest.py
+++ conftest.py
@@ -1,9 +1,29 @@
+from turtle import update
+
 import pytest
 from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_settings_git():
-    browser.config.window_width = '1920'
-    browser.config.window_height = '1080'
-    browser.config.timeout = 4
+    # browser.config.window_width = '1920'
+    # browser.config.window_height = '1080'
+    # browser.config.timeout = 4
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
